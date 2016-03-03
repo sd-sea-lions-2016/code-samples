0a095706bee91c668996fd139d5b07387647a275
Torey Hickman <torey@toreyhickman.com>
2016-03-03 14:54:05 -0800

    Extract method to calculate index to check

--- binary-search/binary_search.rb
+++ binary-search/binary_search.rb
@@ -1,14 +1,18 @@
 def binary_search(target, collection, min_index = 0, max_index = collection.size - 1)
   return nil if collection.empty? || max_index < min_index
 
-  middle_index = (min_index + max_index) / 2
+  index_to_check = mid_point(min_index, max_index)
 
   return case
-         when collection[middle_index] == target
-           middle_index
-         when collection[middle_index] < target
+         when collection[index_to_check] == target
+           index_to_check
+         when collection[index_to_check] < target
            binary_search(target, collection, (min_index + 1), max_index)
-         when collection[middle_index] > target
-           binary_search(target, collection, min_index, (middle_index - 1))
+         when collection[index_to_check] > target
+           binary_search(target, collection, min_index, (index_to_check - 1))
          end
+end
+
+def mid_point(min, max)
+  (min + max) / 2
 end
