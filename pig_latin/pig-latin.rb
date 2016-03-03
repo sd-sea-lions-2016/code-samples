6a305cb28d41ff4eb7875cf936d9a139780dda1c
Torey Hickman <torey@toreyhickman.com>
2016-03-03 15:15:56 -0800

    Refactor pig latin

--- pig_latin/pig-latin.rb
+++ pig_latin/pig-latin.rb
@@ -1,33 +1,34 @@
-class PigLatinizer
-  def convert(string)
-    string.split.map { |x| pigify(x) }.join(" ")
+VOWEL_AT_START = /\A[aeiou]/i
+
+A_CAPITAL_LETTER = /[A-Z]/
+
+PARTS_OF_THE_WORD = %r{
+  \A
+  (?<lead_punctuation>\W+)?
+  (?<lead_consonants>[^aeiou]+)?
+  (?<base>\w*?)
+  (?<contraction>\'\w+\z|n\'t)?
+  (?<end_punctuation>\W+)?
+  \z
+}xi
+
+def convert(string)
+  string.split.map { |individual_word| pigify(individual_word) }.join(" ")
+end
+
+def pigify(word)
+  return word if starts_with_vowel?(word)
+
+  word.sub(PARTS_OF_THE_WORD) do
+    "#{$~[:lead_punctuation]}#{correct_capitalization($~[:base] + $~[:lead_consonants])}ay#{$~[:contraction]}#{$~[:end_punctuation]}"
   end
+end
 
-  def pigify(word)
-    return word if word =~ VOWELS_AT_START
+def correct_capitalization(word)
+  return word unless word.match(A_CAPITAL_LETTER)
+  word.downcase.capitalize
+end
 
-    word.sub PARTS_OF_WORD do
-      "#{$~[:lead_punc]}#{correct_capitalization($~[:base] + $~[:lead_consonants])}ay#{$~[:contraction]}#{$~[:end_punc]}"
-    end
-  end
-
-  private
-  VOWELS_AT_START = /\A[aeiou]/i
-
-  CAPITAL_LETTER = /[A-Z]/
-
-  PARTS_OF_WORD = %r{
-    \A
-    (?<lead_punc>\W+)?
-    (?<lead_consonants>[^aeiou]+)?
-    (?<base>\w*?)
-    (?<contraction>\'\w+\z|n\'t)?
-    (?<end_punc>\W+)?
-    \z
-  }xi
-
-  def correct_capitalization(word)
-    return word unless word =~ CAPITAL_LETTER
-    word.downcase.capitalize
-  end
+def starts_with_vowel?(word)
+  !!word.match(VOWEL_AT_START)
 end
--- pig_latin/spec/pig-latin_spec.rb
+++ pig_latin/spec/pig-latin_spec.rb
@@ -1,69 +1,95 @@
 require_relative '../Pig-latin'
 
-describe PigLatinizer do
-  let(:latinizer) { PigLatinizer.new }
+describe "Converting English to Pig Latin" do
 
   describe "#convert" do
     it "returns pig latinized single words" do
-      expect(latinizer.convert "abc").to eq "abc"
+      expect(convert "abc").to eq "abc"
     end
 
     it "returns pig latinized sentences" do
       string = "\"That shouldn't work,\" I'd said."
-      expect(latinizer.convert string).to eq "\"Atthay ouldshayn't orkway,\" I'd aidsay."
+      expect(convert string).to eq "\"Atthay ouldshayn't orkway,\" I'd aidsay."
     end
   end
 
   describe "#pigify" do
-
     it "doesn't change strings starting with a vowel" do
-      expect(latinizer.pigify "abc").to eq "abc"
-      expect(latinizer.pigify "ebc").to eq "ebc"
-      expect(latinizer.pigify "ibc").to eq "ibc"
-      expect(latinizer.pigify "obc").to eq "obc"
-      expect(latinizer.pigify "Ubc").to eq "Ubc"
+      expect(pigify "abc").to eq "abc"
+      expect(pigify "ebc").to eq "ebc"
+      expect(pigify "ibc").to eq "ibc"
+      expect(pigify "obc").to eq "obc"
+      expect(pigify "Ubc").to eq "Ubc"
     end
 
     it "handles strings beginning with a single consonant" do
-      expect(latinizer.pigify "bac").to eq "acbay"
+      expect(pigify "bac").to eq "acbay"
     end
 
     it "handles strings beginning with multiple consonants" do
-      expect(latinizer.pigify "bbbac").to eq "acbbbay"
+      expect(pigify "bbbac").to eq "acbbbay"
     end
 
     it "preserves punctuation at the beginning of strings" do
-      expect(latinizer.pigify "\"bac").to eq "\"acbay"
+      expect(pigify "\"bac").to eq "\"acbay"
     end
 
     it "preserves puncuation at the end end of strings" do
-      expect(latinizer.pigify "abc\"").to eq "abc\""
+      expect(pigify "abc\"").to eq "abc\""
     end
 
     it "preserves multiple punctuation marks" do
-      expect(latinizer.pigify "something,\"").to eq "omethingsay,\""
+      expect(pigify "something,\"").to eq "omethingsay,\""
     end
 
     it "preserves capitalization at the beginning of words" do
-      expect(latinizer.pigify "Bac").to eq "Acbay"
+      expect(pigify "Bac").to eq "Acbay"
     end
 
     it "preserves all capitalization and punctuation" do
-      expect(latinizer.pigify "\"Toad\"").to eq "\"Oadtay\""
+      expect(pigify "\"Toad\"").to eq "\"Oadtay\""
     end
 
     context "with contractions" do
       it "it preserves n't" do
-        expect(latinizer.pigify "couldn't").to eq "ouldcayn't"
+        expect(pigify "couldn't").to eq "ouldcayn't"
       end
 
       it "preserves 'd" do
-        expect(latinizer.pigify "he'd").to eq "ehay'd"
+        expect(pigify "he'd").to eq "ehay'd"
       end
 
       it "preserves 's" do
-        expect(latinizer.pigify "king's").to eq "ingkay's"
+        expect(pigify "king's").to eq "ingkay's"
       end
+    end
+  end
+
+  describe "#correct_capitalization" do
+    it "returns the word if it contains no capital letters" do
+      expect(correct_capitalization("abc")).to eq "abc"
+    end
+
+    it "returns the word if it starts with a capital letter" do
+      expect(correct_capitalization("Abc")).to eq "Abc"
+    end
+
+    it "moves capitalization to the first letter if another letter is capitalized" do
+      expect(correct_capitalization("aBc")).to eq "Abc"
+    end
+  end
+
+  describe "#starts_with_vowel?" do
+    it "returns true if the word starts with a lowercase a,e,i,o, or u" do
+      expect(starts_with_vowel?("echo")).to be true
+    end
+
+    it "returns true if the word starts with a capital A,E,I,O, or U" do
+      expect(starts_with_vowel?("Ideas")).to be true
+    end
+
+    it "returns false if the word starts with a consonant" do
+      expect(starts_with_vowel?("nighthawks")).to be false
     end
   end
 end
