8873766dda2cf6d40f8d8ef5dd03fc4c01874f26
Mark Siemers <mark.siemers@gmail.com>
2016-08-25 09:13:59 -0700

    Live code for Salamanders

--- sinatra-crud-app/app/controllers/index.rb
+++ sinatra-crud-app/app/controllers/index.rb
@@ -1,61 +1,58 @@
 # Implement RESTful routes for contacts app
-
 get '/search' do
-  # Return a search form
   erb :'contacts/search_form'
 end
 
 get '/results' do
-  # Display results from search
-  puts params
-  cleansed_search_attributes = params[:contact].select {|key, value| !value.empty? }
-  @contacts = Contact.where(cleansed_search_attributes)
+  params[:contact].reject!{|key,value| value.empty? }
+  @contacts = Contact.where(params[:contact])
   erb :'contacts/index'
 end
 
 # index
 get '/contacts' do
-  # Return all contacts
-  @contacts = Contact.all.order(:last_name)
-  puts @contacts
+  @contacts = Contact.all
   erb :'contacts/index'
 end
 
 # new
 get '/contacts/new' do
-  # Return form for a new contact
   @contact = Contact.new
   erb :'contacts/new'
 end
 
 # create
 post '/contacts' do
-  # Create a new contact
-  puts params[:contact]
-  contact = Contact.create(params[:contact])
-  redirect to "/contacts/#{contact.id}"
+  @contact = Contact.new(params[:contact])
+  if @contact.save
+    redirect '/contacts'
+  else
+    status 422
+    erb :'contacts/new'
+  end
 end
 
 # show
 get '/contacts/:id' do
-  # Get a contact with specified id
   @contact = Contact.find(params[:id])
   erb :'contacts/show'
 end
 
 # edit
 get '/contacts/:id/edit' do
-  # Return form for editing an existing contact
   @contact = Contact.find(params[:id])
   erb :'contacts/edit'
 end
 
 # update
 patch '/contacts/:id' do
-  # Update a contact
-  contact = Contact.find(params[:id])
-  contact.update_attributes(params[:contact])
-  redirect to "/contacts/#{contact.id}"
+  @contact = Contact.find(params[:id])
+  if @contact.update_attributes(params[:contact])
+    redirect "/contacts/#{@contact.id}"
+  else
+    status 422
+    erb :'contacts/edit'
+  end
 end
 
 # delete confirmation
@@ -66,13 +63,14 @@ end
 
 # delete
 delete '/contacts/:id' do
-  # Delete a contact
-  puts "I am in the delete action"
-  puts params
-  Contact.find(params[:id]).destroy!
-  redirect to '/contacts'
+  @contact = Contact.find(params[:id])
+  if @contact.destroy
+    redirect '/contacts'
+  else
+    status 422
+    erb :'contacts/delete'
+  end
 end
-
 
 get '/' do
   redirect to '/contacts'
--- sinatra-crud-app/app/models/contact.rb
+++ sinatra-crud-app/app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   # Remember to create a migration!
-
+  validates_presence_of :first_name
   # What validations would you create here?
 end
